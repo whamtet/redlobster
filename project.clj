ae1a5d3f34ce2af169383c3527ee08bb1f38896e
Bodil Stokke <bodil@bodil.tv>
2012-12-02 03:11:08 +0100

    Set a proper license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.bodil/redlobster "0.1.0"
   :description "Promises for Node"
   :url "https://github.com/bodil/redlobster"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Apache License, version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.2.9"]
             [org.bodil/lein-noderepl "0.1.1"]]
