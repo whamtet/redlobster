61838d004f8bf676adf5a6cc9526b9841994110c
Bodil Stokke <bodil@bodil.tv>
2013-01-02 18:36:29 +0100

    Support Ace's event system as well as Node's, allowing events and promises to work in the browser.

--- src/redlobster/events.cljs
+++ src/redlobster/events.cljs
@@ -1,9 +1,24 @@
-(ns redlobster.events
-  (:require-macros [cljs.node-macros :as n])
-  (:use [cljs.node :only [log]]
-        [cljs.yunoincore :only [clj->js]]))
+(ns redlobster.events)
 
-(n/require "events" [EventEmitter])
+(defn- detect-implementation []
+  (try
+    ;; Try Node's EventEmitter
+    {:emitter (.-EventEmitter (js/require "events"))
+     :type :node}
+    (catch js/Error e
+      (try
+        ;; Try Ace's EventEmitter using RequireJS
+        (let [ace-emitter (js/require "ace/lib/event")
+              emitter (fn [])]
+          (aset emitter "prototype" ace-emitter)
+          {:emitter emitter
+           :type :ace})
+        (catch js/Error e
+          (throw (js/Error. "No supported EventEmitter found")))))))
+
+(let [emitter (detect-implementation)]
+  (def EventEmitter (:emitter emitter))
+  (def emitter-type (:type emitter)))
 
 (defn event-emitter []
   (EventEmitter.))
@@ -15,27 +30,46 @@
   (remove-all-listeners [emitter])
   (remove-all-listeners [emitter event])
   (listeners [emitter event])
-  (emit [emitter event args]))
+  (emit [emitter event data]))
 
 (defn- unpack-event [event]
   (if (keyword? event)
     (name event)
     event))
 
-(extend-protocol IEventEmitter
-  EventEmitter
-  (on [emitter event listener]
-    (.on emitter (unpack-event event) listener))
-  (once [emitter event listener]
-    (.once emitter (unpack-event event) listener))
-  (remove-listener [emitter event listener]
-    (.removeListener emitter (unpack-event event) listener))
-  (remove-all-listeners [emitter]
-    (.removeAllListeners emitter))
-  (remove-all-listeners [emitter event]
-    (.removeAllListeners emitter (unpack-event event)))
-  (listeners [emitter event]
-    (js->clj (.listeners emitter (unpack-event event))))
-  (emit [emitter event args]
-    (.apply (.-emit emitter) emitter
-            (apply array (cons (unpack-event event) args)))))
+(case emitter-type
+  :node
+  (extend-protocol IEventEmitter
+   EventEmitter
+   (on [emitter event listener]
+     (.on emitter (unpack-event event) listener))
+   (once [emitter event listener]
+     (.once emitter (unpack-event event) listener))
+   (remove-listener [emitter event listener]
+     (.removeListener emitter (unpack-event event) listener))
+   (remove-all-listeners [emitter]
+     (.removeAllListeners emitter))
+   (remove-all-listeners [emitter event]
+     (.removeAllListeners emitter (unpack-event event)))
+   (listeners [emitter event]
+     (js->clj (.listeners emitter (unpack-event event))))
+   (emit [emitter event data]
+     (.emit emitter (unpack-event event) data)))
+
+  :ace
+  (extend-protocol IEventEmitter
+   EventEmitter
+   (on [emitter event listener]
+     (.on emitter (unpack-event event) listener))
+   (once [emitter event listener]
+     (throw "ace.lib.event_emitter.EventEmitter doesn't support the `once` method."))
+   (remove-listener [emitter event listener]
+     (.removeListener emitter (unpack-event event) listener))
+   (remove-all-listeners [emitter]
+     (throw "ace.lib.event_emitter.EventEmitter doesn't support the `remove-all-listeners` method without an event argument."))
+   (remove-all-listeners [emitter event]
+     (.removeAllListeners emitter (unpack-event event)))
+   (listeners [emitter event]
+     (throw "ace.lib.event_emitter.EventEmitter doesn't support the `once` method."))
+  (emit [emitter event data]
+    (._emit emitter (unpack-event event) data))))
